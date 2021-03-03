63f38cfbd34b5e3e81d19dae9a92ba9d64bcfb63
YH3041 <mmj2rv9@gmail.com>
2021-03-03 20:44:17 +0900

    feat: PostsRepository 추가
    PostsRepository 게시글 저장 및 불러오기 기능 추가 + 테스트 코드 작성

---
+++ src/test/java/com/younghoon/awswebservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.younghoon.awswebservice.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("mmj2rv9")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
